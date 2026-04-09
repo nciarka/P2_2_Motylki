1a83fb66e9ed230389785f27bfcd8dc4f0343583
Natalia Ciarka <nataliaciarka2001@gmail.com>
2026-04-09 19:15:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data1.csv")
+df = pd.read_csv("data/data1.csv")
 #Filtrowanie lat 2000-2016
 df_filtered = df[(df["year"] >= 2000) & (df["year"] <= 2016)]
 
